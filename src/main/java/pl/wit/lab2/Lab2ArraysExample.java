be89274dd2bf0c4eaa4e4d5cc89880f301491a09
Tomek Michalski <tmm.michalski@gmail.com>
2022-04-05 16:34:28 +0200

    add constructors

--- src/main/java/pl/wit/lab2/Lab2ArraysExample.java
+++ src/main/java/pl/wit/lab2/Lab2ArraysExample.java
@@ -1,10 +1,35 @@
 package pl.wit.lab2;
 
+import java.util.Arrays;
+
 public class Lab2ArraysExample {
     private int intNumbers[] = null;
     private String stringElements[] = null;
     private Boolean boolElements[] = null;
 
+    private Lab2ArraysExample () {
+        intNumbers = new int[0];
+        stringElements = new String[0];
+        boolElements = new Boolean[0];
+    }
+
+    private Lab2ArraysExample (int intValue) {
+        this();
+        intNumbers = Arrays.copyOf(intNumbers, 1);
+        intNumbers[0] = intValue;
+    }
+
+    private Lab2ArraysExample (String stringValue) {
+        this();
+        stringElements = Arrays.copyOf(stringElements, 1);
+        stringElements[0] = stringValue;
+    }
+
+    private Lab2ArraysExample (Boolean boolValue) {
+        this();
+        boolElements = Arrays.copyOf(boolElements, 1);
+        boolElements[0] = boolValue;
+    }
 
     public int[] getIntNumbers() {
         return intNumbers;
