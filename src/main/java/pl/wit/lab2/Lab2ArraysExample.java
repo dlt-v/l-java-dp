4adced2c9bba78df0ea792c14b3147c4010cea5c
Tomek Michalski <tmm.michalski@gmail.com>
2022-04-05 15:17:13 +0200

    add getters and setters

---
+++ src/main/java/pl/wit/lab2/Lab2ArraysExample.java
@@ -0,0 +1,32 @@
+package pl.wit.lab2;
+
+public class Lab2ArraysExample {
+    private int intNumbers[] = null;
+    private String stringElements[] = null;
+    private Boolean boolElements[] = null;
+
+
+    public int[] getIntNumbers() {
+        return intNumbers;
+    }
+
+    public void setIntNumbers(int[] intNumbers) {
+        this.intNumbers = intNumbers;
+    }
+
+    public String[] getStringElements() {
+        return stringElements;
+    }
+
+    public void setStringElements(String[] stringElements) {
+        this.stringElements = stringElements;
+    }
+
+    public Boolean[] getBoolElements() {
+        return boolElements;
+    }
+
+    public void setBoolElements(Boolean[] boolElements) {
+        this.boolElements = boolElements;
+    }
+}
